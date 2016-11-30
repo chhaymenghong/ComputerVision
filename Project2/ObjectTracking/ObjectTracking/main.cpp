906846646365ecedd93e24ba9d17ed739d99e6f9
chhaymenghong <chhaymenghong@yahoo.com>
2016-11-30 15:13:14 -0800

    add ranging for hsv images

--- Project2/ObjectTracking/ObjectTracking/main.cpp
+++ Project2/ObjectTracking/ObjectTracking/main.cpp
@@ -1,13 +1,43 @@
 #include "opencv2/opencv.hpp"
-#include<opencv\highgui.h>
 
 using namespace cv;
+
+
+
+void trackBar(int, void*){};
 
 int main(int, char)
 {
 	Mat image;
+	Mat grayImage;
+	Mat hsvImage;
+	Mat thresholdImage;
 	VideoCapture capture;
 	capture.open(0);
+
+	char TrackbarName[10];
+	namedWindow("select color", 0);
+	int hMin = 0;
+	int hMax = 256;
+
+	int sMin = 0;
+	int sMax = 256;
+
+	int vMin = 0;
+	int vMax = 256;
+	//sprintf(TrackbarName, "min H", hMin);
+	//sprintf(TrackbarName, "max H", hMax);
+
+	createTrackbar("Min H", "select color", &hMin, hMax, trackBar);
+	createTrackbar("Max H", "select color", &hMax, hMax, trackBar);
+
+	createTrackbar("Min S", "select color", &sMin, sMax, trackBar);
+	createTrackbar("Max S", "select color", &sMax, sMax, trackBar);
+
+	createTrackbar("Min V", "select color", &vMin, vMax, trackBar);
+	createTrackbar("Min V", "select color", &vMax, vMax, trackBar);
+
+
 	if (!capture.isOpened()) {
 		return -1;
 	}
@@ -17,6 +47,14 @@ int main(int, char)
 		if (waitKey(10) >= 0) {
 			return 0;
 		}
+		//cvtColor(image, grayImage, CV_RGB2GRAY);
+		//imshow("Gray Image", grayImage);
+
+		cvtColor(image, hsvImage, COLOR_BGR2HSV);
+		//imshow("HSV image", hsvImage);
+
+		inRange(hsvImage, Scalar(hMin, sMin, vMin), Scalar(hMax, sMax, vMax), thresholdImage);
+		imshow("ThresholdImage", thresholdImage);
 		
 	}
 	return 0;
